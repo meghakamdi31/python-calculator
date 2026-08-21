29b80dc6317bc69fa3f13310eb71e1e83f5fee71
meghakamdi31 <meghakamdi57@gmail.com>
2026-08-21 23:27:02 +0530

    hello

---
+++ en.py
@@ -0,0 +1,22 @@
+class Person:
+
+    def __init__(self):
+        self.name = "megha"
+        self.age = 22
+
+p1 = Person()
+
+print(p1.name)
+print(p1.age)
+
+
+class Course:
+    def __init__(self):
+        self.course_name = "DA"
+        self.code = 7894
+
+p1 = Course()
+
+print(p1.course_name)
+print(p1.code)
+
